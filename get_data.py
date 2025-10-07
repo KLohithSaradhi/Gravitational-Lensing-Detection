19ad4f7a694fb599bb3ffef3c211a21f6114c333
Lohith <lohith21110257@snuchennai.edu.in>
2025-10-07 15:38:29 -0700

    .py scripts for downloading data

---
+++ get_data.py
@@ -0,0 +1,57 @@
+from astroquery.sdss import SDSS
+from astropy import coordinates as coords
+import astropy.units as u
+import requests
+import pandas as pd
+import os
+import tqdm
+import argparse
+
+SDSS.clear_cache()
+query_bands = ["u", "g", "r", "i", "z"]
+
+def download_data(reference_path, parent_dir):
+    CATALOG = pd.read_csv(reference_path)
+    sub_folder = reference_path.split("/")[-1][:-4]
+
+    dest_path = os.path.join(parent_dir, sub_folder)
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
+
+    QUERY_FAIL = []
+    IMAGE_FAIL = []
+    for i in tqdm.tqdm(range(len(CATALOG))):
+        ra, dec = CATALOG.iloc[i]["ra"], CATALOG.iloc[i]["dec"]
+
+        pos = coords.SkyCoord(ra, dec, unit="deg", frame="icrs")
+        result = SDSS.query_region(pos, radius=2* u.arcsec)
+
+        #FITS
+        if result:
+            for b in query_bands:
+                try:
+                    imgs = SDSS.get_images(coordinates=pos, radius=20*u.arcsec, band=b, data_release=17)
+                    imgs[0].writeto(os.path.join(dest_path, f"{b}_{i+1:06}.fits"), overwrite=True)
+                except:
+                    print("image not found :", i)
+                    IMAGE_FAIL += [i]
+        else:
+            print("query fail :", i)
+            QUERY_FAIL += [i]
+
+        #JPEG
+        url = f"https://skyserver.sdss.org/dr17/SkyServerWS/ImgCutout/getjpeg?ra={ra}&dec={dec}&scale=0.4&width=512&height=512"
+        r = requests.get(url)
+        open(os.path.join(dest_path, f"{i+1:06}.jpg"), "wb").write(r.content)   
+    
+    return QUERY_FAIL, IMAGE_FAIL
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-r","--ref_csv")
+    parser.add_argument("-d","--dest_path")
+
+    args = parser.parse_args()
+
+    download_data(args.ref_csv, args.dest_path)
